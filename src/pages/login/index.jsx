3b33653cd407100fbf24759aeb51f98987d05846
sejineeee <sejinee0918@gmail.com>
2022-07-13 11:49:52 +0900

    feature: 로그인 기능 구현 및 스타일 작업

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,34 +2,59 @@ import React from 'react';
 import '../../styles/login/login.scss'
 import { useState, useRef } from 'react';
 
-
 const Login = () => {
   const adminRef = useRef();
   const passwordRef = useRef();
 
-  const [state, setState] = useState('false');
-  console.log(state);
-  console.log(setState);
+  const [inputs, setInputs] = useState({})
+
+  const onChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const onSubmit = () => {
+
+    if(inputs['admin-id'].length >=5 && inputs['admin-pwd'].length >= 5) {
+      alert('관리자님 로그인 되었습니다.')
+    } else {
+      alert('5글자 이상인지 확인해주세요.')
+    }
+  };
 
   return (
-    <div className='login-container'>
-      <div className='left-container'>
-        <a href="/">ZETAPLAN 홈페이지로</a>
-        <div className='text'>
-          <p>반갑습니다</p>
-          <p>ZETAPLAN입니다</p>
+    <div className='loginContainer'>
+      <div className='logLeftContainer'>
+        <div className='logText'>
+          <p className='logTextFirst'>반갑습니다</p>
+          <p className='logTextSecond'>ZETAPLAN입니다</p>
         </div>
+        <a className="loginLogo" href="/">ZETAPLAN 홈페이지로</a>
       </div>
-      <div className='right-container'>
-        <form className='login-form'>
+      <div className='logRightContainer'>
+        <form className='loginForm'>
           <label htmlFor="admin-id">
-            <input className="id-input" name="admin-id" type="text" placeholder='아이디' ref={adminRef} />
+            <input 
+              className="logIdInput" 
+              name="admin-id" 
+              type="text" 
+              placeholder='아이디' 
+              ref={adminRef}
+              onChange={onChange} />
           </label>
           <label htmlFor="admin-pwd">
-            <input className="pwd-input" type="admin-pwd" placeholder='비밀번호' ref={passwordRef} />
+            <input 
+              className="logPwdInput" 
+              type="password"
+              name="admin-pwd" 
+              placeholder='비밀번호' 
+              ref={passwordRef} 
+              onChange={onChange}/>
           </label>
+          <button className="loginBtn"type="button" onClick={onSubmit}>로그인</button>
         </form>
-        <button className="login-btn" type="button">로그인</button>
       </div>
     </div>
   );
