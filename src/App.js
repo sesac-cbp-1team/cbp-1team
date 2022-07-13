d93d7228c187a87829e3121a5e766c2c3f7b3f78
kimmjiwon <k980912@naver.com>
2022-07-13 12:04:11 +0900

    global 마크업 및 스타일 적용

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import IrPerfo from './pages/ir/IrPerfo';
 import IrPlan from './pages/ir/IrPlan';
 import IrProcess from './pages/ir/IrProcess';
 import Ir from './pages/ir/index';
+import Gbindex from './pages/global/index';
+import GbStrategy from './pages/global/GbStrategy';
+import GbCase from './pages/global/GbCase';
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
           <Route path="/irPerfo" element={<IrPerfo />} />
           <Route path="/irPlan" element={<IrPlan />} />
           <Route path="/irProcess" element={<IrProcess />} />
+          <Route path="/Gbindex" element={<Gbindex />} />
+          <Route path="/GbStrategy" element={<GbStrategy />} />
+          <Route path="/GbCase" element={<GbCase />} />
         </Routes>
       </BrowserRouter>
     </div>
